4f9c1ff90f6959b48795201bb43bfded1a4d66e5
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-02-19 15:20:38 +0700

    Getting Form Access via a Global Key

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -11,6 +11,11 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,76 +25,82 @@ class _NewItemState extends State<NewItem> {
         body: Padding(
           padding: EdgeInsets.all(12),
           child: Form(
+              key: _formKey,
               child: Column(
-            children: [
-              TextFormField(
-                  maxLength: 50,
-                  decoration: InputDecoration(label: Text('Name')),
-                  validator: (value) {
-                    if (value == null ||
-                        value.isEmpty ||
-                        value.trim() == 1 ||
-                        value.length > 50) {
-                      return "Must be between 1 and 50 characters";
-                    }
-                    return null;
-                  }),
-              const SizedBox(
-                width: 8,
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Expanded(
-                    child: TextFormField(
-                        decoration:
-                            const InputDecoration(label: Text("Quantity")),
-                        initialValue: '1',
-                        validator: (value) {
-                          if (value == null ||
-                              value.isEmpty ||
-                              int.tryParse(value) == null ||
-                              int.tryParse(value)! <= 0) {
-                            return 'Must be a valid, positive number.';
-                          }
-                          return null;
-                        }),
+                  TextFormField(
+                      maxLength: 50,
+                      decoration: InputDecoration(label: Text('Name')),
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.trim() == 1 ||
+                            value.length > 50) {
+                          return "Must be between 1 and 50 characters";
+                        }
+                        return null;
+                      }),
+                  const SizedBox(
+                    width: 8,
                   ),
-                  Expanded(
-                    child: DropdownButtonFormField(items: [
-                      for (final category in categories.entries)
-                        DropdownMenuItem(
-                            value: category.value,
-                            child: Row(
-                              children: [
-                                Container(
-                                  width: 16,
-                                  height: 16,
-                                  color: category.value.color,
-                                ),
-                                const SizedBox(
-                                  width: 6,
-                                ),
-                                Text(category.value.title)
-                              ],
-                            ))
-                    ], onChanged: (value) {}),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                            decoration:
+                                const InputDecoration(label: Text("Quantity")),
+                            keyboardType: TextInputType.number,
+                            initialValue: '1',
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  int.tryParse(value) == null ||
+                                  int.tryParse(value)! <= 0) {
+                                return 'Must be a valid, positive number.';
+                              }
+                              return null;
+                            }),
+                      ),
+                      Expanded(
+                        child: DropdownButtonFormField(items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                                value: category.value,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 16,
+                                      height: 16,
+                                      color: category.value.color,
+                                    ),
+                                    const SizedBox(
+                                      width: 6,
+                                    ),
+                                    Text(category.value.title)
+                                  ],
+                                ))
+                        ], onChanged: (value) {}),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                          onPressed: () {
+                            _formKey.currentState!.reset();
+                          },
+                          child: const Text("Reset")),
+                      ElevatedButton(
+                          onPressed: _saveItem, child: const Text("Add Item"))
+                    ],
                   )
                 ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(onPressed: () {}, child: const Text("Reset")),
-                  ElevatedButton(
-                      onPressed: () {}, child: const Text("Add Item"))
-                ],
-              )
-            ],
-          )),
+              )),
         ));
   }
 }
